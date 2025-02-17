511a7cba38b304cdd5868e5408d958de153e3a39
Lucas Xavier <contato@lsx.dev.br>
2025-02-17 15:33:59 -0300

    feat: adjust of key

--- config/apis/google/index.ts
+++ config/apis/google/index.ts
@@ -1,13 +1,10 @@
-import fs from "fs";
 import { drive_v3, google } from 'googleapis';
-import path from "path";
 
 export async function GoogleAuthApi () {
-  const keyPath = path.join(process.cwd(), "public/suu-fotos.key.json");
-    const credentials = JSON.parse(fs.readFileSync(keyPath, "utf-8"));
+  const googleKey = process.env.GOOGLE_KEY as string;
     
     return new google.auth.GoogleAuth({
-      credentials,
+      credentials: JSON.parse(googleKey),
       scopes: ["https://www.googleapis.com/auth/drive.readonly",'https://www.googleapis.com/auth/drive.file'],
     });
 }
