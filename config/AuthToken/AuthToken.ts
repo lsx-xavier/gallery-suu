0fac0ca33500f417254aa8b00a76ab6d8ac77a94
Lucas Xavier <contato@lsx.dev.br>
2025-02-19 22:53:57 -0300

    feat: add more security to access the folders and cookies to access again

--- config/AuthToken/AuthToken.ts
+++ config/AuthToken/AuthToken.ts
@@ -13,7 +13,6 @@ export function verifyToken(token: string) {
   try {
     return jwt.verify(token, SECRET_KEY);
   } catch (err: unknown) {
-    // @ts-expect-error - Because we doesn't have the type of this error.
-    console.log(err)
+    throw err
   }
 }
