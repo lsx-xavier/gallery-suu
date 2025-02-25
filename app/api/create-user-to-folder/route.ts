b72a15782303b792bad58fa7c42fe5fc145558ab
Lucas Xavier <contato@lsx.dev.br>
2025-02-25 19:59:44 -0300

    feat: add GET to see the last upset at create the user to folder

--- app/api/create-user-to-folder/route.ts
+++ app/api/create-user-to-folder/route.ts
@@ -1,4 +1,5 @@
 import createUserToFolder from "@/app/services/create-user-to-folder";
+import { redis } from "@/config/redis";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
@@ -15,5 +16,20 @@ export async function POST(req: Request) {
     });
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
+
+export async function GET() {
+  try {
+    const status = await redis.hgetall('auth:status');
+    const all = await redis.keys('auth:*');
+
+    const allFindedLessStatus = all.filter(a => !a.includes('status'))
+    console.log({ allFindedLessStatus }, allFindedLessStatus.length)
+
+    return NextResponse.json({ status });
+  } catch (err) {
+    console.error('[create-user-to-folder - API] Error getting status:', err);
+    return null;
   }
 }
